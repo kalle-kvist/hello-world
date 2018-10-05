2af87c3b989c0538ccb94368c04a361c5f3c5c3a
kalle-kvist <43893876+kalle-kvist@users.noreply.github.com>
2018-10-06 00:40:32 +0200

    Testfiles from cloud9

---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+
+//*** DEMO FUNCTION PHP ***
+function myfunction()
+{
+    //Some Code Here
+    
+    return "result";
+}
+
+
+//*** CHECKINPUTS ***/
+function checkinputs($fieldname, &$status, &$value)
+{
+    if(isset($_POST[$fieldname])) 
+    {
+        if(!empty($_POST[$fieldname])) 
+        {
+            $value = trim(htmlspecialchars($_POST[$fieldname]));
+        }
+        else
+        {
+            $status = "Enter value";   
+        }                            
+    }
+}
+
+//*** READPOSTARRAY ***/
+function readpostarray()
+{
+    $tmpOutput="";
+    
+    foreach($_POST as $value) 
+    {
+        if (is_array($value)) 
+        {
+            foreach($value as $innerValue ) 
+            {
+              $tmpOutput .= " " . trim(htmlspecialchars($innerValue));
+            }
+        }
+    }
+    
+    return $tmpOutput;
+}
+
+?>
